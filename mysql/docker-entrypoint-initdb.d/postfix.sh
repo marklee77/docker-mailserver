6c1ef7e8ab9de77cadb9c1795c577d87585b0538
Mark Stillwell <mark@stillwell.me>
2015-12-10 16:47:22 +0000

    changed environment variable format...

--- mysql/docker-entrypoint-initdb.d/postfix.sh
+++ mysql/docker-entrypoint-initdb.d/postfix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mysql --protocol=socket -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS \`postfix\`;
-CREATE USER 'postfix'@'%' IDENTIFIED BY '$MYSQL_POSTFIX_DB_PASSWORD';
+CREATE USER 'postfix'@'%' IDENTIFIED BY '$MYSQL_POSTFIX_PASSWORD';
 GRANT ALL ON \`postfix\`.* TO 'postfix'; 
 EOF
 
