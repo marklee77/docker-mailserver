56e954be09e53a1c7dd512e741cbadae734ab642
Mark Stillwell <mark@stillwell.me>
2015-12-10 16:44:26 +0000

    added first db creation script...

---
+++ mysql/docker-entrypoint-initdb.d/postfix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mysql --protocol=socket -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
+CREATE DATABASE IF NOT EXISTS \`postfix\`;
+CREATE USER 'postfix'@'%' IDENTIFIED BY '$MYSQL_POSTFIX_DB_PASSWORD';
+GRANT ALL ON \`postfix\`.* TO 'postfix'; 
+EOF
+
+
