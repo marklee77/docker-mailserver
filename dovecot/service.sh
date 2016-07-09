cda16b44ba800bbae2e49a16daf9bc5e05897ece
Mark Stillwell <mark@stillwell.me>
2016-07-10 00:54:33 +0100

    evolving...

--- dovecot/service.sh
+++ dovecot/service.sh
@@ -4,6 +4,7 @@
 : ${mailserver_ssl_key_file:=/etc/ssl/private/ssl-cert-snakeoil.key}
 
 SOLR_URL="http://${SOLR_PORT_8983_TCP_ADDR}:${SOLR_PORT_8983_TCP_PORT}/solr/dovecot"
+NETWORK=$(ip a s eth0 | sed -nr '/^\s*inet ([^\s]+).*/{s//\1/p;q}')
 
 cat > /etc/cron.daily/dovecot-solr-optimize <<EOF
 #!/bin/bash
@@ -11,32 +12,54 @@ curl $SOLR_URL/update?optimize=true &>/dev/null
 EOF
 chmod 755 /etc/cron.daily/dovecot-solr-optimize
 
+cat > /etc/cron.daily/dovecot-expunge <<EOF
+#!/bin/bash
+doveadm expunge -A mailbox Spam savedbefore 60d
+doveadm expunge -A mailbox Trash savedbefore 60d
+EOF
+chmod 755 /etc/cron.daily/dovecot-expunge
+
 cat > /etc/cron.d/dovecot <<EOF
 * * * * * curl $SOLR_URL/update?commit=true &>/dev/null
 EOF
 
+cat > /etc/dovecot/dovecot-ldap.conf.ext <<EOF
+uri = $LDAP_URL
+tls = yes
+auth_bind = yes
+pass_attrs = uid=user, userPassword=password, \
+  homeDirectory=userdb_home, uidNumber=userdb_uid, gidNumber=userdb_gid
+
+# For LDA: needed?
+# user_attrs = homeDirectory=userdb_home, uidNumber=userdb_uid, gidNumber=userdb_gid
+EOF
+chmod 600 /etc/dovecot/dovecot-ldap.conf.ext
+
 cat > /etc/dovecot/dovecot.conf <<EOF
 protocols = imap pop3 sieve lmtp
 
-ssl = no
+ssl = required
+# FIXME: verify cipher list
 ssl_cipher_list = ALL:!LOW:!SSLv2:ALL:!aNULL:!ADH:!eNULL:!EXP:RC4+RSA:+HIGH:+MEDIUM
 ssl_cert = <$mailserver_ssl_cert_file
 ssl_key = <$mailserver_ssl_key_file
 
 auth_mechanisms = plain login
 auth_username_format = %Ln
-
-disable_plaintext_auth = no
+disable_plaintext_auth = yes
+login_trusted_networks = 127.0.0.0/8 $NETWORK
 
 passdb {
-  driver = pam
+  driver = ldap
+  args = /etc/dovecot/dovecot-ldap.conf.ext
 }
 
 userdb {
-  driver = passwd
+  driver = static
+  args = uid=vmail gid=vmail home=/var/vmail/%u
 }
 
-mail_location = maildir:/var/mail/%u
+mail_location = maildir:~/Maildir
 
 namespace inbox {
   inbox = yes
@@ -55,18 +78,21 @@ namespace inbox {
     special_use = \Sent
   }
   mailbox Spam {
-    auto = subscribe
+    auto = create
     special_use = \Junk
   }
   mailbox Trash {
-    auto = subscribe
+    auto = create
     special_use = \Trash
   }
 }
 
-mail_plugins = fts fts_solr
+mail_plugins = fts fts_solr mailbox_alias
 
 plugin {
+
+  mailbox_alias_old = Archive
+  mailbox_alias_new = Sent
 
   # antispam configuration
   antispam_backend = dspam
@@ -89,8 +115,8 @@ plugin {
   fts_solr = break-imap-search url=$SOLR_URL/
 
   # sieve configuration
-  sieve = ~/.dovecot.sieve
-  sieve_dir = ~/.sieve.d
+  sieve = ~/sieve.default
+  sieve_dir = ~/sieve.d
 
 }
 
@@ -103,6 +129,7 @@ service auth {
 
 # local delivery
 service lmtp {
+  user = vmail
   inet_listener {
     port = 8025
   }
