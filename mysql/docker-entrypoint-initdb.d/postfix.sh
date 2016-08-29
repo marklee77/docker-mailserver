bdbdcc27315af947dba4489c42c63d2849702627
Mark Stillwell <mark@stillwell.me>
2016-08-29 01:05:13 +0100

    make mysql command a little more clear..

--- mysql/docker-entrypoint-initdb.d/postfix.sh
+++ mysql/docker-entrypoint-initdb.d/postfix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mysql --protocol=socket -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
+mysql --protocol=socket --user=root --password="$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS \`postfix\`;
 CREATE USER 'postfix'@'%' IDENTIFIED BY '$MYSQL_POSTFIX_PASSWORD';
 GRANT ALL ON \`postfix\`.* TO 'postfix';
