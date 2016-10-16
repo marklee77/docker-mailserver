0d1f1d1d456807a6a6f12257a4d70bf18c77fd6e
Mark Stillwell <mark@stillwell.me>
2016-10-17 00:57:10 +0100

    cleaning up, planning...

--- mysql/docker-entrypoint-initdb.d/postfix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mysql --protocol=socket --user=root --password="$MYSQL_ROOT_PASSWORD" <<EOF
-CREATE DATABASE IF NOT EXISTS \`postfix\`;
-CREATE USER 'postfix'@'%' IDENTIFIED BY '$MYSQL_POSTFIX_PASSWORD';
-GRANT ALL ON \`postfix\`.* TO 'postfix';
-EOF
