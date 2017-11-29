c205cd1d4a56664feec23731736d559447474400
Mark Stillwell <mark@stillwell.me>
2017-11-29 23:35:43 +0000

    remove dkim, sqlgrey from postfix and update solr

--- postfix/root/usr/local/sbin/opendkim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-: ${postfix_ldap_basedn:=dc=ldap,dc=dit}
-
-# do not start opendkim before ldap is available
-while ! ldapsearch -Z -D cn=postfix,ou=dsa,$postfix_ldap_basedn -y /etc/ldap/ldap.passwd >/dev/null 2>&1; do sleep 1; done
-
-exec /usr/sbin/opendkim -f
--- postfix/root/usr/local/sbin/sqlgrey.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /etc/sqlgrey/psql-env.sh
-
-# do not start sqlgrey before database is available
-while ! psql <<< "\\q" >/dev/null 2>&1; do sleep 1; done
-
-exec /usr/sbin/sqlgrey
