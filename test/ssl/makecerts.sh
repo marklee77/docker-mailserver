fa60e543ea7080f3b8960babcaa03dd1cc8a256d
Mark Stillwell <mark@stillwell.me>
2017-12-10 13:25:16 +0000

    make temporary ca for testing
    needs some cleanup

---
+++ test/ssl/makecerts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir -p csrs certs private newcerts
+touch index.txt
+echo 1000 >serial
+openssl req -config ./openssl.conf -newkey rsa:4096 -x509 -nodes -days 365 \
+    -subj "/CN=ca" -sha256 -extensions v3_ca \
+    -out certs/ca.cert.pem -keyout private/ca.key.pem
+
+for S in ldap dovecot replica; do
+    openssl req -config ./openssl.conf -newkey rsa:4096 -nodes -days 365 \
+        -subj "/CN=${S}" -out "csrs/${S}.csr.pem" -keyout "private/${S}.key.pem"
+    openssl ca -config ./openssl.conf -batch -md sha256 -days 365 \
+        -extensions server_cert \
+        -in "csrs/${S}.csr.pem" -out "certs/${S}.cert.pem"
+done
